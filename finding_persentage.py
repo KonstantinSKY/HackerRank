275f0ab0647e1cac1fe0de5a02f80749d74fc265
Konstantin Smolnikov <sky012877@gmail.com>
2020-10-31 00:44:42 -0700

    solved

---
+++ finding_persentage.py
@@ -0,0 +1,12 @@
+"""https://www.hackerrank.com/challenges/finding-the-percentage/problem"""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        line = input().split()
+        name, scores = line[0], line[1:]
+        scores = list(map(float, scores))
+        student_marks[name] = scores
+    query_name = input()
+    print('{:.2f}'.format(sum(student_marks[query_name]) / len((student_marks[query_name]))))
