5f206b73e200367a9bc443cec3393d28d681f086
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-01 07:50:13 -0800

    Find_string.py solved

---
+++ Find_string.py
@@ -0,0 +1,16 @@
+"""https://www.hackerrank.com/challenges/find-a-string/problem"""
+
+
+def count_substring(string, sub_string):
+    i = count = 0
+
+    while i <= len(string):
+        i = string.find(sub_string, i) + 1
+        if i:
+            count += 1
+        else:
+            return count
+
+
+if __name__ == '__main__':
+    print(count_substring(input().strip(), input().strip()))
